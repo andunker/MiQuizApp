ab01317c4743ef14d1c15e5ec3cfa39a40cf855f
Fabio Mora <andunker@hotmail.com>
2023-08-07 20:02:34 -0500

    add questions

--- app/src/main/java/com/thegeekchief/myquizapp/MainActivity.kt
+++ app/src/main/java/com/thegeekchief/myquizapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.thegeekchief.myquizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +20,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val etName: EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+
+            if (etName.text.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Please enter your name", Toast.LENGTH_LONG
+                ).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
     }
 }
 
